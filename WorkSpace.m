79098a5e1de65e5e2d7175ce46b43d6ce7e58359
Aly Khaled <alykhaled2001@live.com>
2021-05-01 16:45:35 +0200

    Greeen

--- WorkSpace.m
+++ WorkSpace.m
@@ -1,34 +1,58 @@
-clear
-clc
-l1 = 4;
-l2 = 3;
-% l3 = 30;
+function [] = WorkSpace(l1,l2,l3,theta1_min,theta1_max,theta2_min,theta2_max,theta3_min,theta3_max)
+t1=linspace(theta1_min, theta1_max, 100);
+t2=linspace(theta2_min, theta2_max, 100);
+t3=linspace(theta3_min, theta3_max, 100);
+[q1,q2,q3]=ndgrid(t1,t2,t3);
+xM = l1 * cosd(q1) + l2 * cosd(q1 + q2) + l3 * cosd(q1 + q2 + q3);
+yM = l1 * sind(q1) + l2 * sind(q1 + q2) + l3 * sind(q1 + q2 + q3);
+plot(xM(:),yM(:),'k.')
 
-q1_min = -90;
-q1_max = 180;
-q2_min = 0;
-q2_max = 120;
-% q3_min = -90;
-% q3_max = 150;
-t1=linspace(q1_min, q1_max, 200)*pi/180;
-t2=linspace(q2_min, q2_max, 200)*pi/180;
-% t3=linspace(q3_min, q3_max, 200)*pi/180;
-
-[q1,q2]=ndgrid(t1,t2); 
-xM = l1 * cos(q1) + l2 * cos(q1 + q2);
-yM = l1 * sin(q1) + l2 * sin(q1 + q2);
-% xM = l1 * cos(q1) + l2 * cos(q1 + q2) + l3 * cos(q1 + q2 + q3);
-% yM = l1 * sin(q1) + l2 * sin(q1 + q2) + l3 * sin(q1 + q2 + q3);
-x = l1 * cos(t1) + l2 * cos(t1 + t2);
-y = l1 * sin(t1) + l2 * sin(t1 + t2);
-plot(xM,yM,'b-')
-area = 0;
-d = xM(4);
-for i = 1:199
-    for j = 1:199
-        area = area + (xM(i,j)+xM(i,j+1))*(yM(i,j+1)-yM(i,j));
-    end
-end
-area = area/180
-
+% xL = xlim; %all values of x 
+% yL = ylim; % all values of y 
+% line(xL, [0 0],'color','b','linewidth',1) %Draw x-axis
+% line([0 0], yL,'color','b','linewidth',1) %Draw y-axis
 grid on
+end
+% l1 = 5;
+% l2 = 4;
+% l3 = 30;
+% 
+% q1_min = 0;
+% q1_max = 180;
+% q2_min = 0;
+% q2_max = 90;
+% % q3_min = -90;
+% % q3_max = 150;
+% 
+% % 
+% 
+% 
+% % x = l1 * cos(t1) + l2 * cos(t1 + t2);
+% % y = l1 * sin(t1) + l2 * sin(t1 + t2);
+% 
+% xN = l1 * cosd(t1) + l2 * cosd(t1 + t2);
+% yN = l1 * sind(t1) + l2 * sind(t1 + t2);
+% % [Xsorted,I] = sort(xM);
+% % Ysorted = yM(I);
+% % area = 0;
+% % for i = 1:499
+% %     for j = 1:499
+% %         area = area + ((Xsorted(i+1,j)- Xsorted(i,j))*(Ysorted(i+1,j) + Ysorted(i,j)));
+% %     end
+% % end
+% 
+% 
+% % for i = q1_min:q1_max-1
+% %     for j = q2_min:q2_max-1
+% %         x = l1 * cosd(i) + l2 * cosd(i + j);
+% %         y = l1 * sind(i) + l2 * sind(i + j);
+% %         x2 = l1 * cosd(i) + l2 * cosd(i + j+1);
+% %         y2 = l1 * sind(i) + l2 * sind(i + j+1);
+% %         area = area + ((x2 + x)*(y2 - y));
+% %     end
+% % end
+% 
+% % area = area/180;
+% % area = area/2
+% 
+% grid on
--- workingArea.m
+++ workingArea.m
@@ -2,48 +2,22 @@ function [area] = workingArea(l1,l2,theta1_min,theta1_max,theta2_min,theta2_max)
 %Working Area using Green Theorem
 %Inputs: L1, L2, Theta 1 min,Theta 1 max, Theta 2 min, Theta2 max
 %Return: The working area
-[v1x,v1y] = dkpm(l1,l2,theta1_min,theta2_min);
-[v2x,v2y] = dkpm(l1,l2,theta1_min,theta2_max);
-[v3x,v3y] = dkpm(l1,l2,theta1_max,theta2_min);
-[v4x,v4y] = dkpm(l1,l2,theta1_max,theta2_max);
 
-v1 = [v1x;v1y];
-v2 = [v2x;v2y];
-v3 = [v3x;v3y];
-v4 = [v4x;v4y];
-% x = [v1x v2x v3x v4x];
-% y = [v1y v2y v3y v4y];
-v = [v1 v2 v3 v4];
-% a = polyarea(x,y);
+% t1=linspace(theta1_min, theta1_max, 500)*pi/180;
+% t2=linspace(theta2_min, theta2_max, 500)*pi/180;
+
 area = 0;
-
-for i = theta1_min:theta1_max
-    for j = theta2_min:theta2_max
-        [x,y] = dkpm(l1,l2,i,j);
-        [x2,y2] = dkpm(l1,l2,i,(j+1));
-        area = area + (x2 + x)*(y2 - y)/2;
+for i = theta1_min:theta1_max-1
+    for j = theta2_min:theta2_max-1
+        x = l1 * cosd(i) + l2 * cosd(i + j);
+        y = l1 * sind(i) + l2 * sind(i + j);
+        x2 = l1 * cosd(i) + l2 * cosd(i + j+1);
+        y2 = l1 * sind(i) + l2 * sind(i + j+1);
+        area = area + ((x2 + x)*(y2 - y));
     end
 end
-
-% for i = theta1_min:theta1_max
-%     [x,y] = dkpm(l1,l2,i,theta2_max);
-%     [x2,y2] = dkpm(l1,l2,i+1,theta2_max);
-%     area = area + (x2 + x)*(y2 - y)/2;
-% end
-% for i = theta2_min:theta2_max
-%     [x,y] = dkpm(l1,l2,theta1_min,i);
-%     [x2,y2] = dkpm(l1,l2,theta1_min,i+1);
-%     area = area + (x2 + x)*(y2 - y)/2;
-% end
-% for i = theta2_min:theta2_max
-%     [x,y] = dkpm(l1,l2,theta1_max,i);
-%     [x2,y2] = dkpm(l1,l2,theta1_max,i+1);
-%     area = area + (x2 + x)*(y2 - y)/2;
-% end
-% for i = 1:3
-%     area = area + (v(1,i) + v(1,i+1))*(v(2,i+1) - v(2,i));
-% end
-% area = a;
+area = area/2;
+area = area*pi/180;
 
 end
 
