52d5a7316a7d6630b8ee00a4a8051f040f5ba8cc
Aly Khaled <alykhaled2001@live.com>
2021-05-09 13:34:46 +0200

    Delete Omar's useless code

--- DKPM_RRR.m
+++
@@ -1,5 +0,0 @@
-function [x, y, phi] = DKPM_RRR(l1, l2, l3,theta1,theta2,theta3)
-x = (l1*cosd(theta1)) + (l2*cosd(theta1 + theta2) + (l3*cosd(theta1 + theta2 + theta3)));
-y = (l1*sind(theta1)) + (l2*sind(theta1 + theta2) + (l3*sind(theta1 + theta2 + theta3)));
-phi = theta1 + theta2 + theta3;
-end
